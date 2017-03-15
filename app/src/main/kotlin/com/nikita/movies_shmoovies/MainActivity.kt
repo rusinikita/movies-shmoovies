d5b2ad346051bf15f5c7e802172e2aff57e8148f
Nikita <rusinikit@gmail.com>
2017-03-15 10:55:47 +0200

    Add coroutines

--- app/src/main/kotlin/com/nikita/movies_shmoovies/MainActivity.kt
+++ app/src/main/kotlin/com/nikita/movies_shmoovies/MainActivity.kt
@@ -4,12 +4,18 @@ import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
 import android.widget.TextView
+import kotlinx.coroutines.experimental.android.UI
+import kotlinx.coroutines.experimental.async
+import kotlinx.coroutines.experimental.delay
+import kotlinx.coroutines.experimental.launch
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
 
   private var mTextMessage: TextView? = null
 
   private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+
     when (item.itemId) {
       R.id.navigation_home -> {
         mTextMessage!!.setText(R.string.title_home)
@@ -17,13 +23,32 @@ class MainActivity : AppCompatActivity() {
       }
       R.id.navigation_dashboard -> {
         mTextMessage!!.setText(R.string.title_dashboard)
+
+        launch(UI) {
+          repeat(10) {
+            mTextMessage?.text = "Repeat $it"
+            delay(1, TimeUnit.SECONDS)
+          }
+        }
+
         return@OnNavigationItemSelectedListener true
       }
       R.id.navigation_notifications -> {
         mTextMessage!!.setText(R.string.title_notifications)
+
+        val async = async(UI) {
+          delay(5, TimeUnit.SECONDS)
+          "Async done!"
+        }
+
+        launch(UI) {
+          mTextMessage?.text = async.await()
+        }
+
         return@OnNavigationItemSelectedListener true
       }
     }
+
     false
   }
 
