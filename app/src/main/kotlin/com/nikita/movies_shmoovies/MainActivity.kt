91ea81392e61467430cd938cb137cd7a856ad429
kholodno <pavel.gvay@gmail.com>
2017-03-29 15:42:27 +0200

    Add tabs

--- app/src/main/kotlin/com/nikita/movies_shmoovies/MainActivity.kt
+++ app/src/main/kotlin/com/nikita/movies_shmoovies/MainActivity.kt
@@ -2,8 +2,12 @@ package com.nikita.movies_shmoovies
 
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
+import android.support.v4.view.ViewCompat
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.view.View
 import com.nikita.movies_shmoovies.common.utils.findView
+import com.nikita.movies_shmoovies.movies.MoviesFragment
 import com.nikita.movies_shmoovies.posters.PostersFragment
 
 class MainActivity : AppCompatActivity() {
@@ -11,7 +15,7 @@ class MainActivity : AppCompatActivity() {
 
     when (item.itemId) {
       R.id.navigation_movies -> {
-        showPostersFragment(PostersFragment.Type.Movies)
+        showMoviesFragment()
         true
       }
       R.id.navigation_tv_shows -> {
@@ -30,12 +34,13 @@ class MainActivity : AppCompatActivity() {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
     setSupportActionBar(findView(R.id.toolbar))
+    supportActionBar?.elevation = 0f
     setTitle(R.string.app_name)
     val navigation = findView<BottomNavigationView>(R.id.navigation)
     // TODO Лень обходить баг с запоминанием выбранной вкладки
     navigation.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
     if (savedInstanceState == null) {
-      showPostersFragment(PostersFragment.Type.Movies)
+      showMoviesFragment()
     }
   }
 
@@ -44,5 +49,12 @@ class MainActivity : AppCompatActivity() {
       .beginTransaction()
       .replace(R.id.fragment_container, PostersFragment.create(type))
       .commit()
+  }
+
+  private fun showMoviesFragment() {
+    supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment_container, MoviesFragment())
+            .commit()
   }
 }
---
+++ app/src/main/kotlin/com/nikita/movies_shmoovies/movies/MoviesFragment.kt
@@ -0,0 +1,52 @@
+package com.nikita.movies_shmoovies.movies
+
+import android.os.Bundle
+import android.support.design.widget.TabLayout
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentStatePagerAdapter
+import android.support.v4.view.ViewPager
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.nikita.movies_shmoovies.R
+import com.nikita.movies_shmoovies.common.utils.findView
+import com.nikita.movies_shmoovies.posters.PostersFragment
+
+class MoviesFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.movies_content, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val tabLayout = view.findView<TabLayout>(R.id.tabLayout)
+        val viewPager = view.findView<ViewPager>(R.id.viewpager)
+        viewPager.adapter = MoviesPagerAdapter(activity.supportFragmentManager)
+        tabLayout.setupWithViewPager(viewPager)
+    }
+
+    class MoviesPagerAdapter(fragmentManager: FragmentManager) : FragmentStatePagerAdapter(fragmentManager) {
+        val TAB_COUNT = 3
+
+        override fun getItem(position: Int): Fragment {
+            when (position) {
+                0 -> return PostersFragment.create(PostersFragment.Type.Movies)
+                1 -> return PostersFragment.create(PostersFragment.Type.TvShows)
+                2 -> return PostersFragment.create(PostersFragment.Type.People)
+                else -> return PostersFragment.create(PostersFragment.Type.Movies)
+            }
+        }
+
+        override fun getPageTitle(position: Int): CharSequence {
+            when (position) {
+                0 -> return "Popular"
+                1 -> return "Upcoming"
+                2 -> return "Top-10"
+                else -> return "Error"
+            }
+        }
+
+        override fun getCount(): Int = TAB_COUNT
+    }
+}
